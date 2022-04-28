bf3b670a6cb254ed6a21236665df00af46a50010
avoic <avoic@DESKTOP-MAB9RL9>
2022-04-28 19:54:13 +0300

    Add project files.

---
+++ Hola mundo/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Hola_mundo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hola Mundo!");
+            Console.WriteLine("Hola Mundo2!");
+            Console.WriteLine("Hola Mundo3!");
+        }
+    }
+}
